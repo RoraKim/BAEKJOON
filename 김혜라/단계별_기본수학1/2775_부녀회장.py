f9983cb3a9b137f8edadda5c159142c861c82ab2
kjasm0714 <kjasm0714@naver.com>
2022-02-15 18:39:19 +0900

    20220215

--- 김혜라/단계별_기본수학1/2775_부녀회장.py
+++ 김혜라/단계별_기본수학1/2775_부녀회장.py
@@ -2,7 +2,18 @@ t = int(input())
 for i in range(t):
     k = int(input())
     n = int(input())
-    arr = [[0] * (k + 1) for _ in range(n + 1)]
-    for i in range(n + 1):
-        arr[0][i] = i
-print(arr)
+    arr = [[0 for _ in range(n + 1)] for _ in range(k + 1)]
+    for i in range(k + 1):
+        # print(i)
+        for j in range(n + 1):
+
+            if i == 0:  
+                arr[i][j] = j
+        
+    # print(arr)
+
+    for i in range(1, k + 1):
+        for j in range(1, n+1):
+            arr[i][j] = arr[i - 1][j] + arr[i][j - 1]
+
+    print(arr[k][n])
